d9844fff69a1e925c453425d14017e1b8f5e5a73
Johnson <64374564+Dawodu-Johnson@users.noreply.github.com>
2023-05-10 14:32:31 +0100

    Create complicated_gcd.cpp

---
+++ complicated_gcd.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    
+    string a,b;
+    cin >> a >> b;
+    
+    if(a == b){
+         cout << a << "\n";
+    }
+    else{
+         cout << 1 << "\n";
+    }
+    return 0;
+}
