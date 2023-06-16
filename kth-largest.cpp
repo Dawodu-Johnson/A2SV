1b59324bfb3cbcc4cc549ac047a946f8f7fa3661
Johnson <64374564+Dawodu-Johnson@users.noreply.github.com>
2023-06-16 18:29:00 +0100

    Create kth-largest.cpp

---
+++ kth-largest.cpp
@@ -0,0 +1,32 @@
+class KthLargest {
+    int size;
+    priority_queue<int,vector<int>,greater<int>>store;
+    
+public:
+    KthLargest(int k, vector<int>& nums) {
+        
+        size=k;
+        for(const int &value: nums)store.push(value);
+        
+        while(store.size() > size){
+            store.pop();
+        }
+        
+    }
+    
+    int add(int val) {
+        
+        store.push(val);
+        if(store.size() >size){
+            store.pop();
+        }
+        
+        return store.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
